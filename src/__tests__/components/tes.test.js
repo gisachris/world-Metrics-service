c0b3089cda4cb3cbd3655622115fc6859d4f888e
gisa chris <gisachris24@gmail.com>
2023-07-06 13:41:01 -0700

    Remove Build Directory From Ignore List

--- src/__tests__/components/tes.test.js
+++
@@ -1,6 +0,0 @@
-import sum from "../../mocks/sum";
-
-test("check if sum works",()=> {
-const answer = sum(2,3);
-expect(answer).toEqual(5);
-})
--- src/mocks/sum.js
+++
@@ -1,2 +0,0 @@
-const sum = (arg1,arg2)=> arg1 + arg2;
-export default sum;
